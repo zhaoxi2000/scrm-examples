da3cafb35faf3f59af94b1eae12c9122eb27fe90
zhaoxi1988 <linzuxiong1988@gmail.com>
2018-01-11 16:23:13 +0800

    CustomerIntroduceService

---
+++ src/main/java/me/zhaoxi/scrm/domain/Customer.java
@@ -0,0 +1,11 @@
+package me.zhaoxi.scrm.domain;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author zhaoxi linzuxiong1988@gmail.com
+ */
+public class Customer {
+    private static final Logger logger = LoggerFactory.getLogger(Customer.class);
+}
---
+++ src/main/java/me/zhaoxi/scrm/domain/CustomerIntroduceService.java
@@ -0,0 +1,20 @@
+package me.zhaoxi.scrm.domain;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.function.Consumer;
+
+/**
+ * 在两个商家直接互相转移客户, 提供导流量的服务.
+ * @author zhaoxi linzuxiong1988@gmail.com
+ */
+public class CustomerIntroduceService {
+    private static final Logger logger = LoggerFactory.getLogger(CustomerIntroduceService.class);
+
+
+    public void introduce(Customer fromCustomer, Business toBusiness){
+
+    }
+
+}
